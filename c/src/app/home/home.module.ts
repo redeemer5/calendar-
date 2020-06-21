08ceee2876ae3cc5212a52c7f582314951e778b1
redeemer matshili <48877861+redeemer5@users.noreply.github.com>
2020-06-21 14:19:40 +0200

    added project

---
+++ c/src/app/home/home.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { RouterModule } from '@angular/router';
+import { NgCalendarModule  } from 'ionic2-calendar';
+
+
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    NgCalendarModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
